f18db2938ead49d92f30f4dbe612e173c3ef26d3
hiengyen <ngyentrnghie777@gmail.com>
2025-07-15 22:22:07 +0700

    fix vpc and add ec2 ssm

--- modules/database/main.tf
+++ modules/database/main.tf
@@ -2,25 +2,28 @@ provider "aws" {
   region = var.aws_region
 }
 
+# RDS CLUSTER
 resource "aws_rds_cluster" "aurora" {
-  cluster_identifier        = "${var.name_prefix}-aurora-cluster"
-  engine                    = "aurora-mysql"
-  engine_version            = "8.0.mysql_aurora.3.08.0"
-  database_name             = "${var.database_name}db"
-  master_username           = var.db_master_username
-  master_password           = var.db_master_password
-  backup_retention_period   = var.backup_retention_period
-  deletion_protection       = var.enable_deletion_protection
-  db_subnet_group_name      = aws_db_subnet_group.main.name
-  vpc_security_group_ids    = [aws_security_group.rds.id]
-  preferred_backup_window   = "07:00-09:00"
-  skip_final_snapshot       = var.skip_final_snapshot
-  final_snapshot_identifier = "${var.name_prefix}-final-snapshot"
-  tags                      = var.tags
+  cluster_identifier              = "${var.name_prefix}-aurora-cluster"
+  engine                          = "aurora-mysql"
+  engine_version                  = "8.0.mysql_aurora.3.08.0"
+  database_name                   = "${var.database_name}db"
+  master_username                 = var.db_master_username
+  master_password                 = var.db_master_password
+  backup_retention_period         = var.backup_retention_period
+  deletion_protection             = var.enable_deletion_protection
+  db_subnet_group_name            = aws_db_subnet_group.main.name
+  vpc_security_group_ids          = [aws_security_group.rds.id, aws_security_group.ssm.id]
+  preferred_backup_window         = "07:00-09:00"
+  skip_final_snapshot             = var.skip_final_snapshot
+  final_snapshot_identifier       = "${var.name_prefix}-final-snapshot"
+  enabled_cloudwatch_logs_exports = ["error", "general", "slowquery"]
+  tags                            = var.tags
 }
 
+# RDS CLUSTER INSTANCE
 resource "aws_rds_cluster_instance" "aurora_instance" {
-  count               = 1
+  count               = 2
   identifier          = "${var.name_prefix}-aurora-instance-${count.index + 1}"
   cluster_identifier  = aws_rds_cluster.aurora.id
   instance_class      = var.instance_class
@@ -30,12 +33,14 @@ resource "aws_rds_cluster_instance" "aurora_instance" {
   tags                = var.tags
 }
 
+# Create a DB subnet group for the RDS cluster
 resource "aws_db_subnet_group" "main" {
   name       = "${var.name_prefix}-db-subnet-group"
   subnet_ids = var.private_subnet_ids
   tags       = var.tags
 }
 
+# Create a security group for the RDS cluster
 resource "aws_security_group" "rds" {
   name_prefix = "${var.name_prefix}-rds-sg-"
   vpc_id      = var.vpc_id
@@ -44,8 +49,8 @@ resource "aws_security_group" "rds" {
     from_port       = 3306
     to_port         = 3306
     protocol        = "tcp"
-    security_groups = [var.ecs_sg_id, var.lambda_sg_id]
-    cidr_blocks     = ["0.0.0.0/0"]
+    security_groups = [var.ecs_sg_id, var.lambda_sg_id, aws_security_group.ssm.id]
+    # cidr_blocks     = ["0.0.0.0/0"]
   }
 
   egress {
@@ -63,6 +68,74 @@ resource "aws_security_group" "rds" {
   )
 }
 
+# Create an IAM role for SSM
+resource "aws_iam_role" "ssm" {
+
+  name = "${var.name_prefix}-ssm-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+# Attach the AmazonSSMManagedInstanceCore policy to the SSM role
+resource "aws_iam_role_policy_attachment" "ssm" {
+  role       = aws_iam_role.ssm.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+# Create an IAM instance profile for EC instances to use SSM
+resource "aws_iam_instance_profile" "ssm" {
+  name = "${var.name_prefix}-ssm-profile"
+  role = aws_iam_role.ssm.name
+}
+
+# Create a security group for EC2 instances to allow SSM access 
+resource "aws_security_group" "ssm" {
+  name   = "${var.name_prefix}-ssm-sg"
+  vpc_id = var.vpc_id
+
+  ingress {
+    from_port = 3306
+    to_port   = 3306
+    protocol  = "tcp"
+    self      = true
+  }
+
+  egress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = var.tags
+}
+
+resource "aws_instance" "ssm_host" {
+  ami                  = data.aws_ami.amazon_linux_2.id
+  instance_type        = "t3.micro"
+  subnet_id            = var.private_subnet_ids[0]
+  security_groups      = [aws_security_group.ssm.id]
+  iam_instance_profile = aws_iam_instance_profile.ssm.name
+
+  tags = merge(
+    var.tags,
+    {
+      Name = "${var.name_prefix}-ssm-host"
+    }
+  )
+}
+
+# DYNAMODB TABLE
 resource "aws_dynamodb_table" "main" {
   name         = "${var.name_prefix}-dynamodb-table"
   billing_mode = "PAY_PER_REQUEST"
@@ -75,3 +148,4 @@ resource "aws_dynamodb_table" "main" {
 
   tags = var.tags
 }
+
